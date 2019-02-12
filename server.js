8081053b62468869b755e9918b29349d77665ea3
Carlos Coello <carlos.coello05@outlook.com>
2019-02-12 01:06:13 -0600

    quick update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connect(
 );
 
 app.use(cors());
-app.use(express.static('dist')
+app.use(express.static("dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
